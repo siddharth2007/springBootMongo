36cb7a8a159e19002ac688526226bd9cb02ae6fb
siddharth2007 <60152139+siddharth2007@users.noreply.github.com>
2022-07-06 10:12:46 +0530

    Update SpringBootMongoApplication.java

--- src/main/java/com/example/springbootmongo/SpringBootMongoApplication.java
+++ src/main/java/com/example/springbootmongo/SpringBootMongoApplication.java
@@ -8,6 +8,8 @@ public class SpringBootMongoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMongoApplication.class, args);
+        System.out.println("Hello World");
     }
+    
 
 }
